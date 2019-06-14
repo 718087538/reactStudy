99b0d122c73165c76656a362a47f2bc3e945e173
kason <718087538@qq.com>
2019-06-14 14:43:39 +0800

    优化todolist

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,4 +1,5 @@
 import React, { Component, Fragment } from 'react';
+import './style.css'
 
 class ToDoList extends Component {
 
@@ -14,10 +15,14 @@ class ToDoList extends Component {
     render() {
         return (
             <Fragment>
+                {/* 注释：花括号代表js的表达式，注释也要这样写在里面，单行则用// */}
+                <label htmlFor = "insertArea">输入内容</label>
+                {/* htmlFor就是for,因为与jsx的for循环冲突，所以替换了，类似的class也要替换成className */}
                 <input
+                id="insertArea"
+                    className="input"
                     value={this.state.inputValue}
                     onChange={this.handleInputChange.bind(this)}
-
                 />
 
                 <button onClick={this.handleBtnClick.bind(this)}>提交</button>
@@ -26,7 +31,12 @@ class ToDoList extends Component {
                         this.state.list.map((item, index) => {
                             return (
                                 <li
-                                    key={index} onClick={this.deleteItem.bind(this,index)}>
+                                    key={index}
+                                    onClick={this.deleteItem.bind(this, index)}
+                                    dangerouslySetInnerHTML={{__html:item}}
+                                    // 上面这种写法可以输出html，但是存在XSS安全漏洞
+                                    >
+                                        {/* item */}
                                     {item}
                                 </li>
                             )
@@ -53,11 +63,11 @@ class ToDoList extends Component {
         console.log(index);//输出点击的下标
         //immutable规范，不允许做state的任何改变，不然没法做性能优化
         const list = [...this.state.list];//所以先展开数组，赋值个一个常量
-        list.splice(index,1);
-        
+        list.splice(index, 1);
+
         //react改变数据必须调用this.setState方法。
         this.setState({
-            list:list
+            list: list
         })
     }
 }
