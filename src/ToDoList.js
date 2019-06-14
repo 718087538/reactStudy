5d40633cf8be05d1c336d6b6228cb0b86d9a8213
kason <718087538@qq.com>
2019-06-14 14:31:52 +0800

    完成todoLIst增加，删除

--- src/ToDoList.js
+++ src/ToDoList.js
@@ -1,42 +1,63 @@
-import React, { Component ,Fragment } from 'react';
+import React, { Component, Fragment } from 'react';
 
 class ToDoList extends Component {
 
-    constructor(props){
+    constructor(props) {
         super(props);
-        this.state ={
-            inputValue:'',
-            list:[],
+        this.state = {
+            inputValue: '',
+            list: [],
         }
     }
-
-
-
 
 
     render() {
         return (
             <Fragment>
                 <input
-                 value={this.state.inputValue} 
-                 onChange={this.handleInputChange.bind(this)}   
-                
+                    value={this.state.inputValue}
+                    onChange={this.handleInputChange.bind(this)}
+
                 />
-                
-                
-                 <button>提交</button>
-               <ul>
-                   <li>学英语</li>
-                   <li>ENGLISH</li>
-               </ul>
+
+                <button onClick={this.handleBtnClick.bind(this)}>提交</button>
+                <ul>
+                    {
+                        this.state.list.map((item, index) => {
+                            return (
+                                <li
+                                    key={index} onClick={this.deleteItem.bind(this,index)}>
+                                    {item}
+                                </li>
+                            )
+                        })
+                    }
+                </ul>
             </Fragment>
         )
     }
 
-    handleInputChange(e){
+    handleInputChange(e) {
         // console.log(e.target.value);
         this.setState({
-            inputValue : e.target.value
+            inputValue: e.target.value
+        })
+    }
+    handleBtnClick(e) {
+        this.setState({
+            list: [...this.state.list, this.state.inputValue],
+            inputValue: ''
+        })
+    }
+    deleteItem(index) {
+        console.log(index);//输出点击的下标
+        //immutable规范，不允许做state的任何改变，不然没法做性能优化
+        const list = [...this.state.list];//所以先展开数组，赋值个一个常量
+        list.splice(index,1);
+        
+        //react改变数据必须调用this.setState方法。
+        this.setState({
+            list:list
         })
     }
 }
