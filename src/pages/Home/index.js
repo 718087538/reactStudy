d707189311a608e2b89c1554c6abfc38668573a6
wangchenming <wangchenming@eunke.com>
2020-05-28 21:53:06 +0800

    引入了Redux

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,6 +1,11 @@
 import React, { Component } from 'react';
 import { Route, Link } from "react-router-dom"
 import Mine from "../Mine"
+import { DatePicker, Input, Button } from 'antd';
+import 'antd/dist/antd.css';
+import './index.css'
+// import './t2.scss'
+
 
 class Home extends Component {
     constructor(props) {
@@ -8,13 +13,22 @@ class Home extends Component {
         this.state = {
             title: "真·首页"
         }
+        this.changeInputValue = this.changeInputValue.bind(this);
+    }
+    changeInputValue(e) {
+        console.log(e.target.value);
     }
     render() {
+
         return (
             <>
+                <DatePicker />
                 <a href='#/mine'>我的</a>
+                <div className="inputArea">
+                    <Input placeholder="Basic usage" onChange={this.changeInputValue} />
+                    <Button type="primary">提交</Button>
+                </div>
                 <h1>现在位于首页</h1>
-
             </>
         )
     }
